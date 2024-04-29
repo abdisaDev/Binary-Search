3c5682d35c5a698e2e99f48492e05b2d76f66209
Abdisa Dev <abdisaDev@gmail.com>
2024-04-29 23:54:38 +0300

    binary search done

--- BinarySearch.ts
+++ BinarySearch.ts
@@ -1,36 +1,19 @@
 class Stack {
-  private stack: Array<number> = [];
-  private stackLength: number = this.stack.length - 1;
-
-  binarySearch(stack: Array<number>, input: number): string {
-    let array: Array<number> = this.sortStack(stack);
+  binarySearch(stack: Array<number>, input: number): boolean {
+    let array: Array<number> = Array.from(new Set(this.sortStack(stack)));
     let mid = Math.floor(array.length / 2);
-    console.log(array[mid], " == ", input);
+
     if (array[mid] === input) {
-      return `${input} found at index mid: ${mid}`;
+      return true;
     } else if (array[mid] < input && array.length > 1) {
-      let leftElements = array.splice(mid, Number.MAX_VALUE);
-      this.binarySearch(leftElements, input);
+      let rightElements = array.splice(mid, Number.MAX_VALUE);
+      return this.binarySearch(rightElements, input);
     } else if (array[mid] > input && array.length > 1) {
-      let rightElements = array.splice(0, mid);
-      console.log(rightElements, "2");
-      this.binarySearch(rightElements, input);
+      let leftElements = array.splice(0, mid);
+      return this.binarySearch(leftElements, input);
+    } else {
+      return false;
     }
-    return `${input} Not found`;
-  }
-
-  push(input: number) {
-    this.stack[this.stackLength - 1] = input;
-  }
-
-  set setStack(array: Array<number>) {
-    for (let i = 0; i < array.length; i++) {
-      this.stack[i] = array[i];
-    }
-  }
-
-  get getStack() {
-    return this.stack;
   }
 
   private sortStack(array: Array<number>): Array<number> {
@@ -38,7 +21,9 @@ class Stack {
 
     try {
       if (array.length === 0) {
-        throw new Error(`Stack is Empty`);
+        throw new Error("Stack is Empty");
+      } else if (array.length < 1) {
+        throw new Error("Stack elements can not be less than 2.");
       }
     } catch (err) {
       console.error(err);
@@ -57,7 +42,44 @@ class Stack {
   }
 }
 
-let stack = new Stack();
-stack.setStack = [1, 4, 5, 2, 3];
-console.log(stack.binarySearch([1, 4, 5, 2, 3], 2));
-class Main {}
+class Main {
+  private stackInstance: Stack;
+  private stack: Array<number> = [];
+  private inputValue: number;
+
+  constructor() {
+    this.stackInstance = new Stack();
+  }
+
+  set setStack(array: Array<number>) {
+    for (let i = 0; i < array.length; i++) {
+      this.stack[i] = array[i];
+    }
+  }
+
+  set input(inputValue: number) {
+    this.inputValue = inputValue;
+  }
+
+  push(element: number) {
+    this.stack[this.stack.length] = element;
+    return this.stack;
+  }
+
+  get searchResult() {
+    const index = `${this.stack.findIndex(
+      (element) => this.inputValue === element
+    )}`;
+
+    return this.stackInstance.binarySearch(this.stack, this.inputValue)
+      ? `${this.inputValue} found at index: ${index}!`
+      : `${this.inputValue} Not found!`;
+  }
+}
+
+const bs = new Main();
+
+bs.setStack = [1, 4, 6, 8, 2, 6, 0];
+bs.push(7);
+bs.input = 9;
+console.log(bs.searchResult);
---
+++ output/BinarySearch.js
@@ -0,0 +1,75 @@
+class Stack {
+    binarySearch(stack, input) {
+        let array = Array.from(new Set(this.sortStack(stack)));
+        let mid = Math.floor(array.length / 2);
+        if (array[mid] === input) {
+            return true;
+        }
+        else if (array[mid] < input && array.length > 1) {
+            let rightElements = array.splice(mid, Number.MAX_VALUE);
+            return this.binarySearch(rightElements, input);
+        }
+        else if (array[mid] > input && array.length > 1) {
+            let leftElements = array.splice(0, mid);
+            return this.binarySearch(leftElements, input);
+        }
+        else {
+            return false;
+        }
+    }
+    sortStack(array) {
+        let swap;
+        try {
+            if (array.length === 0) {
+                throw new Error("Stack is Empty");
+            }
+            else if (array.length < 1) {
+                throw new Error("Stack elements can not be less than 2.");
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+        for (let i = 0; i < array.length; i++) {
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    swap = array[i];
+                    array[i] = array[j];
+                    array[j] = swap;
+                }
+            }
+        }
+        return array;
+    }
+}
+class Main {
+    stackInstance;
+    stack = [];
+    inputValue;
+    constructor() {
+        this.stackInstance = new Stack();
+    }
+    set setStack(array) {
+        for (let i = 0; i < array.length; i++) {
+            this.stack[i] = array[i];
+        }
+    }
+    set input(inputValue) {
+        this.inputValue = inputValue;
+    }
+    push(element) {
+        this.stack[this.stack.length] = element;
+        return this.stack;
+    }
+    get searchResult() {
+        const index = `${this.stack.findIndex((element) => this.inputValue === element)}`;
+        return this.stackInstance.binarySearch(this.stack, this.inputValue)
+            ? `${this.inputValue} found at index: ${index}!`
+            : `${this.inputValue} Not found!`;
+    }
+}
+const bs = new Main();
+bs.setStack = [1, 4, 6, 8, 2, 6, 0];
+bs.push(7);
+bs.input = 9;
+console.log(bs.searchResult);
